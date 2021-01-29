786c9009589bcb6f446bbf38763ff03c9500b11d
Musa-Husseini <musa.husseini@wsu.edu>
2021-01-29 15:39:46 -0800

    First PA1 commit

---
+++ PA1/cmake-build-debug/CMakeFiles/CPTS_223_PA-executable.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CPTS_223_PA-executable.dir/factorial.cpp.o"
+  "CMakeFiles/CPTS_223_PA-executable.dir/main.cpp.o"
+  "CMakeFiles/CPTS_223_PA-executable.dir/message.cpp.o"
+  "CPTS_223_PA-executable"
+  "CPTS_223_PA-executable.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CPTS_223_PA-executable.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ PA1/cmake-build-debug/CMakeFiles/CPTS_223_PA-executable.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for CPTS_223_PA-executable.
+# This may be replaced when dependencies are built.
